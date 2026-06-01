0709abad48898773dcd16b91f8ed25c135908bda
starlord <dhirajkr1117@gmail.com>
2026-06-02 03:42:17 +0530

    updating app.js

--- backEnd/app.js
+++ backEnd/app.js
@@ -9,9 +9,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const routes = require('./routes/bookRouter')
+
 app.get('/',(req,res)=>{
     res.send('Hey there welcome to my server!')
 })
+
+app.use('/books',routes)
 
 db.sync({alter:true})
 .then(()=>{
